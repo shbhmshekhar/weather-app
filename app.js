b3387628d6d9cb8a10ae8cd82b4d59fc89abe180
shbhmshekhar <shbhmshekhar@outlook.com>
2021-10-11 18:36:15 +0530

    added geocode api to get latitude and longitude

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ request({url: url, json: true}, (error, response) =>{
     console.log('=========================================================\n');
     console.log(`${response.body.current.weather_descriptions[0]} Temperature is ${response.body.current.temperature}C and there is ${response.body.current.precip}% chance of rain and it feels like ${response.body.current.feelslike}C`);
 } )
+
+const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2hiaG1zaGVraGFyIiwiYSI6ImNrdW1sbXlidjFya2sydm96dWNyZjRsamoifQ.t4xUuL7X9EAkpLaiCRxXpA&limit=1';
+
+request({url: url1, json: true}, (error, response)=> {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+
+    console.log(latitude, longitude);
+})

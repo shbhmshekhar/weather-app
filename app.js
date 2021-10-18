f38b211f0e88eebae44b54ca49bf0ee2d4317086
shbhmshekhar <shbhmshekhar@outlook.com>
2021-10-18 13:41:09 +0530

    functionality to provide location from cmd

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const request = require('request');
 const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+//const yargs = require('yargs');
 
-geoCode('Bangalore', (error, data) => {
-    if(error){
-        return console.log(error);
-    }
-    console.log('data for :' ,data.location);
-    forecast(data.latitude, data.longitude, (error, data) => {
+
+const location = process.argv[2]
+
+if(location){
+    geoCode(location, (error, data) => {
         if(error){
-           return console.log(error);
-        }            
-        console.log(data)
-    })
-});
+            return console.log(error);
+        }
+        console.log('data for : ', data.location);
+        forecast(data.latitude, data.longitude, (error, data) => {
+            if(error){
+               return console.log(error);
+            }            
+            console.log(data)
+        })
+    });
+} else {
+    console.log('Please provide a location for which forecast is required.')
+}
+
+
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,21 +1,16 @@
 const request = require('request');
 
-const forecast = (coordinates, callback) => {
+const forecast = (latitude, longitude, callback) => {
 
-    const latitude = coordinates.latitude;
-    const longitude = coordinates.longitude;
-
-    const url = `http://api.weatherstack.com/current?access_key=f7d7ef1f2763ec5d2ee728a3af67caa5&query=${latitude},${longitude}&units=m`
+    const url = 'http://api.weatherstack.com/current?access_key=f7d7ef1f2763ec5d2ee728a3af67caa5&query=' + latitude +','+ longitude;
 
     request({url: url, json: true}, (error, response) => {
         if(error){
             callback('Unable to connect to weather service', undefined)
         } else if(response.body.error){
-            callback('Unable to find location', undefined);
+            callback('Unable to find location...bad coordinates', undefined);
         }else{
-            callback(undefined, {temperature: response.body.current.temperature, 
-                feelsLike: response.body.current.feelslike, 
-                rainProb: response.body.current.precip})
+            callback(undefined, `${response.body.current.weather_descriptions[0]} outside. Temperature is currently ${response.body.current.temperature} degrees out and feels like ${response.body.current.feelslike}`)
         }
     })
 }

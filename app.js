b66c6d3fa92e874545f17ede3baa2666726faf0a
shbhmshekhar <shbhmshekhar@outlook.com>
2021-10-18 13:29:25 +0530

    Implemented callback chaining

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const request = require('request');
 const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geoCode('Darbhanga', (error, data) => {
+geoCode('Bangalore', (error, data) => {
     if(error){
-        console.log(error);
-    } else {
-        console.log(data)
+        return console.log(error);
     }
+    console.log('data for :' ,data.location);
+    forecast(data.latitude, data.longitude, (error, data) => {
+        if(error){
+           return console.log(error);
+        }            
+        console.log(data)
+    })
 });
 
-forecast({latitude: 26.16667, longitude: 85.9}, (error, data) => {
-    console.log(error);
-    console.log(data);
-})
